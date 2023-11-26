540adb6af069dda813091050cca9bf51f4609059
nanda.kista <nanda.kista@gmail.com>
2023-11-26 07:27:15 +0700

    Set Crash display from redscreen to custom view

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,7 @@ import 'package:skybase/core/localization/locale_manager.dart';
 import 'config/themes/app_theme.dart';
 import 'service_locator.dart';
 import 'ui/routes/app_routes.dart';
+import 'ui/views/404_500/crash_error_view.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -49,6 +50,12 @@ class App extends StatelessWidget {
             routeInformationParser: AppRoutes.router.routeInformationParser,
             routeInformationProvider: AppRoutes.router.routeInformationProvider,
             routerDelegate: AppRoutes.router.routerDelegate,
+            builder: (BuildContext context, child) {
+              ErrorWidget.builder = (FlutterErrorDetails error) {
+                return CrashErrorView(errorDetails: error);
+              };
+              return child ?? const SizedBox.shrink();
+            },
           );
         },
       ),
